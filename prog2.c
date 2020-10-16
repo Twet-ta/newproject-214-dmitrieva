0f9af98839013ec8e2b87be945afecfa47cf19de
Twet-ta <rain.illusive@mail.ru>
2020-10-16 15:08:02 +0300

    Local

--- prog2.c
+++ prog2.c
@@ -2,87 +2,104 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
- 
-#define MAXLEN 50
+
 int sum = 0;
- 
+
 struct tree{
-    char arr[MAXLEN];
+    char *arr;
     int num;
     struct tree *left;
     struct tree *right;
 };
- 
+
 struct list{
-    char arr[MAXLEN];
+    char *arr;
     int num;
     float ch;
     struct list *nextlist;
 };
- 
-void addtree (struct tree **head, char *word){
+
+void addtree (struct tree **head, char *word, int len){
     if ((*head) == NULL){
         (*head) = malloc(sizeof(struct tree));
-        strcpy((*head)->arr, word);
+        (*head)->arr = malloc(len);
+        strncpy((*head)->arr, word, len);
         (*head)->num = 1;
         (*head)->left = (*head)->right = NULL;
     }
     else{
         if (strcmp((*head)->arr, word) < 0){
-            addtree(&((*head)->left), word);
+            addtree(&((*head)->left), word, len);
         }
         else{
             if (strcmp((*head)->arr, word) == 0) {
                 (*head)->num++;
             }
             else{
-                addtree(&((*head)->right), word);
+                addtree(&((*head)->right), word, len);
             }
         }
     }
 }
- 
+
 void getwords (FILE *f, struct tree **h){
-    int count = 0, c;
-    char words[MAXLEN];
-    while ((c = fgetc(f)) != EOF){
+    int count = 0, size = 25, c;
+    char *words = NULL; 
+    char *symb = NULL; 
+	while ((c = fgetc(f)) != EOF){
+        if (!count) {
+            size = 25;
+            words = malloc(size);
+        }
+        if(size <= count){
+            size *= 2;
+            words = realloc(words,size);
+        }
         if (c == ' ' || c == '\n') {
             if (count) {
                 words[count] = '\0';
-                addtree(h, words);
+                addtree(h, words, count+1);
                 sum++;
             }
+            free(words);
+            words = NULL;
             count = 0;
         }
         else{
             if (isalpha(c)){
-                words[count] = c;
+                words[count] = (char) c;
                 count++;
             }
             else {
                 if (count != 0) {
                     words[count] = '\0';
-                    addtree(h, words);
+                    addtree(h, words, count+1);
                     sum++;
                 }
-                if (!isdigit(c)){
-                    words[0] = c;
-                    words[1] = '\0';
-                    addtree(h, words);
+                if (ispunct(c)){
+                    symb = malloc(2);
+                    symb[0] = c;
+                    symb[1] = '\0';
+                    addtree(h, symb, 2);
+                    free(symb);
+                    symb = NULL;
                     sum++;
                 }
+                free(words);
+                words = NULL;
                 count = 0;
             }
         }
     }
     if (count) {
         words[count] = '\0';
-        addtree(h, words);
+        addtree(h, words, count+1);
+        free(words);
+        words = NULL;
         sum++;
     }
-    count = 0;
 }
- 
+
 ///ищет нужное место для слова из head
 void addlist(struct tree **head, struct list **point){
     struct list **nod;
@@ -92,6 +109,7 @@ void addlist(struct tree **head, struct list **point){
     chmain /= sum;
     if ((*point) == NULL) {
         *point = malloc(sizeof(struct list));
+        (*point)->arr = malloc(strlen((*head)->arr)+1);
         strcpy((*point)->arr, (*head)->arr);
         (*point)->ch = chmain;
         (*point)->num = (*head)->num;
@@ -103,13 +121,14 @@ void addlist(struct tree **head, struct list **point){
             nod1 = malloc(sizeof(struct list));
             nod1->nextlist = *point;
             *point = nod1;
+            nod1->arr = malloc(strlen((*head)->arr)+1);
             strcpy(nod1->arr, (*head)->arr);
             nod1->ch = chmain;
             nod1->num = (*head)->num;
         }
     }
 }
- 
+
 ///перебирает слова из дерева
 void createlist(struct tree *head, struct list **point){
     if (head){
@@ -118,31 +137,36 @@ void createlist(struct tree *head, struct list **point){
         createlist((head->right), point);
     }
 }
- 
+
 void printlist(struct list *l, FILE *f){
     if (l){
         fprintf(f,"%s %d %f\n", l->arr, l->num, l->ch);
         printlist((l->nextlist), f);
     }
 }
- 
+
 void freetree(struct tree *head){
     if (head != NULL){
         freetree(head->left);
         freetree(head->right);
+        free(head->arr);
+        head->arr = NULL;
         free(head);
+        head = NULL;
     }
 }
- 
+
 void freelist(struct list *l){
     if (l) {
         freelist(l->nextlist);
+        free(l->arr);
+        l->arr = NULL;
         free(l);
+        l = NULL;
     }
 }
- 
+
 int main(int argc, char *argv[]) {
-    int c;
     FILE *fin;
     fin = stdin;
     FILE *fout;
@@ -158,5 +182,8 @@ int main(int argc, char *argv[]) {
     printlist(finlist, fout);
     freetree(h);
     freelist(finlist);
+    fclose(fin);
+    fclose(fout);
     return 0;
 }
+
