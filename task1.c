9ba57410a2c2cea802d61347d54f215daf9c438b
Twet-ta <70687237+Twet-ta@users.noreply.github.com>
2020-12-08 15:01:40 +0300

    Update task1.c

--- task1.c
+++ task1.c
@@ -7,5 +7,24 @@
 #include <fcntl.h>
 #include <signal.h>
 
-int main(){
+int main(int argc,char *argv[]){
+    if (argc == 5){
+         int fo = 0, fd[2], status;
+         fo = open(argv[4], O_CREAT | O_APPEND | O_RDONLY);
+         pipe(fd);
+         pid = fork();
+         if(!pid){ //child
+             dup2();
+             _exit(0);
+         }
+         else{
+             waitpid(pid, &status, 0);
+             if (WIFEXITED(status)){
+             }
+         }
+         
+    }
+    else{
+        fprintf(stderr, "Error");
+    }
 }
