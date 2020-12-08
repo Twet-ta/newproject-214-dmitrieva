c2905d0d255035b39c9762b94cdddb24f8a73787
Twet-ta <70687237+Twet-ta@users.noreply.github.com>
2020-12-08 16:18:56 +0300

    inter

--- task2.c
+++ task2.c
@@ -1,16 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <sys/wait.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
 #include <fcntl.h>
-#include <signal.h>
+#include <stdlib.h>
+
 
 int
 main(int argc, char *argv[])
 {
-    if (argc != 2) return 0;
+    int fd1 = open(argv[1], O_RDWR);
+    int fd2 = open(argv[1], O_RDWR);
+
+    int s, count = 0;
+    int dig_count = 0, str_count;
+    while (read(fd1, &s, 1) > 0) {
+        if (s == '\n') {
+            if (dig_count == 2) {
+                //lseek()
+            }
+            str_count = 0;
+            dig_count = 0;
+        } else if (s <= '9' && s >= '0'){
+            dig_count++;
+        }
+        count++;
+        str_count++;
+        write(fd2, &s, 1);
+    }
+    count++;
+    s = '\0';
+    write(fd2, &s, 1);
     
     return 0;
 }
