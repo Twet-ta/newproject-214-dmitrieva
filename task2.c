0907ef62ff7d25cd39959de595a626fad4ecf10c
Twet-ta <70687237+Twet-ta@users.noreply.github.com>
2020-12-08 16:50:53 +0300

    final

--- task2.c
+++ task2.c
@@ -11,13 +11,16 @@ main(int argc, char *argv[])
     int fd1 = open(argv[1], O_RDWR);
     int fd2 = open(argv[1], O_RDWR);
 
-    int s, count = 0;
+    int s, count = 0, curr_count = 0;
     int dig_count = 0, str_count = 0;
+    int lose = 0;
     while (read(fd1, &s, 1) > 0) {
         if (s == '\n') {
             if (dig_count == 2) {
-                lseek(fd2, SEEK_SET, count-str_count);
-                printf("%d %d\n", count, str_count);
+                lseek(fd2, curr_count-str_count - 1, SEEK_SET);
+                //printf("%d %d\n", count, str_count);
+                lose += str_count;
+                curr_count -= (str_count + 1);
             }
             str_count = 0;
             dig_count = 0;
@@ -28,13 +31,13 @@ main(int argc, char *argv[])
             str_count++;
         }
         write(fd2, &s, 1);
+        curr_count++;
         count++;
     }
-
     count++;
     s = '\0';
     write(fd2, &s, 1);
-    ftruncate(fd2, count);
+    ftruncate(fd2, curr_count);
     close(fd1);
     close(fd2);
     return 0;
