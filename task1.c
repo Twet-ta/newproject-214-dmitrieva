1fdf6508ddb2e080cfdce95de54c130ad46b8b26
Twet-ta <70687237+Twet-ta@users.noreply.github.com>
2020-12-08 10:13:26 +0300

    Inter

---
+++ task1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <signal.h>
+
+int main(){
+}
