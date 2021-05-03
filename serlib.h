e8ce2203e8a5d8d410973aaa00ae04ba170ef548
Twet-ta <70687237+Twet-ta@users.noreply.github.com>
2021-05-03 12:02:24 +0300

    Update serlib.h

--- serlib.h
+++ serlib.h
@@ -1,7 +1,3 @@
-//
-// Created by twetta on 16.04.2021.
-//
-
 #ifndef SERVER_SERLIB_H
 #define SERVER_SERLIB_H
 
@@ -15,10 +11,18 @@
 #include <vector>
 #include <cstdint>
 #include <fcntl.h>
-using namespace std;
+#include <sys/wait.h>
+#include <errno.h>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::string;
+using std::to_string;
 
 const int BACKLOG = 5; //queue
-#define _PORT 8001
+#define _PORT 8000
 #define _BUFF 1024
 #define _IP "127.0.0.1"
 
@@ -100,7 +104,7 @@ vector<string> split_lines(string str){
 }
 
 //vector<uint8_t> string_to_vector(string& str){
-    //vector<uint8_t> v;
+//vector<uint8_t> v;
 //}
 
 class connected_socket_ : public socket_{ //stop
@@ -118,11 +122,23 @@ public:
         if (recv(sd, buf, _BUFF, 0) < 0) cout << "Bad reading in connected socket" << endl;
         str = buf;
     };
+    void read_(vector<uint8_t>& bytes){
+        char buf[_BUFF];
+        if (recv(sd, buf, _BUFF, 0) < 0) cout << "Bad reading in connected socket" << endl;
+        for (int i = 0; buf[i]; ++i) {
+            bytes.push_back(buf[i]);
+        }
+    };
+    int get_sd(){ return sd;}
 };
 
 class client_socket_ : public connected_socket_{
 public:
-    void connect(const socket_address& servaddr);
+    client_socket_() : connected_socket_() {}; //check
+    void connect_(const socket_address& saddr){
+        int len = sizeof(saddr);
+        if(connect(sd, saddr.get_addr(), len) < 0) perror("Bad connecting with server") ;
+    };
 };
 
 string get_path(string str) {
@@ -137,156 +153,145 @@ vector<uint8_t> file_to_vect(int fd){
     vector<uint8_t> temp;
     char c;
     while(read(fd, &c, 1)) temp.push_back(c);
+    temp.push_back(0);
     return temp;
 }
-
-void data_connection(connected_socket_ cs, string path) {
-    int fd;
-    cout << "conn path: " << path << endl;
-    if (path.length() == 0) path = "index.html";
-    if ((fd = open(path.c_str(), O_RDONLY)) < 0) {
-        cs.write_("HTTP/1.1 404 Not Found\0");
-        cout << "HTTP/1.1 404 Not Found" << endl;
-        if ((fd = open("404.html", O_RDONLY)) < 0) {
-            cout << "404 not found" << endl;
-        }
-    } else {
-        cs.write_("HTTP/1.1 200 OK\0");
-        cout << "HTTP/1.1 200 OK" << endl;
+void vect_to_file (int fd, const std::vector<uint8_t> &v) {
+    for (uint8_t b : v) {
+        write(fd, reinterpret_cast<const void *>(b), sizeof(b));
     }
-    vector<uint8_t> v = file_to_vect(fd);
-    string str = "\r\nVersion: HTTP/1.1\r\nContent-length: " + to_string(v.size()) + "\r\n\r\n";
-    cout << "Version: " << "HTTP/1.1" << endl;
-    cout << "Content-length: " << to_string(v.size()) << endl;
-    cs.write_(str);
-    cs.write_(v);
-    cs.shutdown_();
-    close(fd);
-}
-#endif //SERVER_SERLIB_H
-
-/*
- *
- * struct dat{
-    char Www[3];
-    int dd;
-    char Mmm[3];
-    int YYYY;
-    int hh;
-    int mm;
-    int ss;
-};
-
-struct date{
-    dat d;
-};
-
-struct last_modified{
-    dat d;
-};
-
-struct host{
-    string name;
-    int port;
-};
-
-struct refer{
-    string uri;
-};
-
-struct user_agent{
-    string name;
-    int ch;
-};
-
-struct server{
-    string name;
-    int ch;
-};
-
-struct content_length{
-    int num;
-};
-
-struct content_type{
-    string type;
-    string subtype;
-};
-
-struct allow{
-    string method = "GET, HEAD";
-};
- void client_connection(){
-    client_socket_ s;
-    socket_address saddr("127.0.0.1", 1555);
-    s.connect(saddr);
-    //make request
-    http_request rq;
-    //make request ...
-    s.write_(rq.to_string());
-    string str_resp;
-    s.read_(str_resp);
-    http_response resp;
-    vector<string> lines = split_;ines(str_resp);
-    // parse responce ...;
-    s.shutdown();
 }
 
-class http_header{
-    string name;
-    string value;
-public:
-    http_header(const string& n, const string& v) : name(n), value(v) {};
-    static http_header* parse_header(const string& line);
-    //explicit http_header(const string& line); // server <-> reqest_header
-    //string to_string() const{...};
-};
+void file_to_file (int fd1, int fd2) {
+    char c;
+    int k = 0;
+    while(read(fd1, &c, 1) != EOF) {
+        write(fd2, &c, 1);
+        k++;
+    }
+}
 
-class http_header_main : http_header{
-    date d;
-};
+char **get_env(const string& fname, const string& query) {
+    char** env = new char*[7];
+    env[0] = new char[14 + query.size()]; // QUERY_STRING
+    strcpy(env[0], "QUERY_STRING=");
+    strcat(env[0], query.c_str());
+    env[1] = new char[22]; // REMOTE_ADDR
+    env[1] = (char *) "REMOTE_ADDR=127.0.0.1";
+    env[2] = new char[24]; // CONTENT_TYPE
+    env[2] = (char *) "CONTENT_TYPE=text/plain"; // CONTENT_TYPE
+    env[3] = new char[26]; // GATEWAY_INTERFACE
+    env[3] = (char *) "GATEWAY_INTERFACE=CGI/1.1"; // GATEWAY_INTERFACE
+    env[4] = new char[17]; // SERVER_PORT
+    env[4] = (char *) "SERVER_PORT=8001"; // SERVER_PORT
+    env[5] = new char[22]; // SERVER_ADDR
+    env[5] = (char *) "SERVER_ADDR=127.0.0.1"; // SERVER_ADDR
+    env[6] = nullptr;
+    return env;
+}
 
-class http_header_essence : http_header{
-    content_length cl;
-    content_type ct;
-    allow a;
-    last_modified lm;
-};
+void data_connection(connected_socket_ cs, string path, const string& request) {
+    int fd;
+    if (path.find('?') == -1) {
+        cout << "conn path: " << path << endl;
+        if (path.length() == 0) path = "index.html";
+        if ((fd = open(path.c_str(), O_RDONLY)) < 0) {
+            cs.write_("HTTP/1.1 404 Not Found\0");
+            cout << "HTTP/1.1 404 Not Found" << endl;
+            if ((fd = open("404.html", O_RDONLY)) < 0) {
+                cout << "404 not found" << endl;
+            }
+        } else {
+            cs.write_("HTTP/1.1 200 OK\0");
+            cout << "HTTP/1.1 200 OK" << endl;
+        }
+        vector<uint8_t> v = file_to_vect(fd);
+        string str = "\r\nVersion: HTTP/1.1\r\nContent-length: " + to_string(v.size()) + "\r\n\r\n";
+        cout << "Version: " << "HTTP/1.1" << endl;
+        cout << "Content-length: " << to_string(v.size()) << endl;
+        cs.write_(str);
+        cs.write_(v);
+        cs.shutdown_();
+        close(fd);
+     } else {
+        int fd1;
+        int status;
+        pid_t pid;
+        cout << "conn path: " << path << endl;
+        if ((pid = fork()) < 0) perror("Bad fork");
+        if (pid > 0) { // parent
+            wait(&status);
+            if(WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+                fd1 = open("log.txt", O_RDONLY);
+                vector<uint8_t> v = file_to_vect(fd1);
+                cout << "Server sends: " << "HTTP/1.1 200 OK" << endl;
+                cs.write_("HTTP/1.1 200 OK\0");
+                string str;
+                str += "\r\nVersion: HTTP/1.1\r\nContent-type: text/html\r\nContent-length: " + to_string(v.size()) + "\r\n\r\n";
+                cout << "Version: " << "HTTP/1.1" << endl;
+                cout << "Content-length: " << v.size() << endl;
+                cs.write_(str);
+                cs.write_(v);
+                cs.shutdown_();
+                close(fd1);
+            }
+        } else if (pid < 0) { perror("Disaster");
+        } else { // child
+            if ((fd1 = open("log.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644)) < 0) perror ("Not making file");
+            dup2(fd1, 1);
+            close(fd1);
+            string fname;
+            int i = 0;
+            while(path[i] != '?') {
+                fname += path[i];
+                i++;
+            }
+            string vars;
+            i = (int) fname.length() + 1;
+            while(i < path.length()) {
+                vars += path[i];
+                i++;
+            }
+            char* argv[] = {(char*)fname.c_str(), nullptr};
 
-class http_header_reqest : http_header{
-    host h;
-    refer r;
-    user_agent ua;
-};
+            char** env = get_env(fname, vars);
+            execve(fname.c_str(), argv, env);
+            string str;
+            switch (errno){
+                case EACCES:
+                    str = "HTTP/1.1 403 Forbidden\n";
+                case ENETRESET:
+                    str = "HTTP/1.1 503 Service Unavailable\n";
+                case EADDRNOTAVAIL:
+                    str = "HTTP/1.1 404 Not Found\n";
+                default:
+                    str = "HTTP/1.1 500 Internal Server Error \n";
+            }
+            cs.write_(str);
+            cout << str << endl;
+            perror("Bad exec");
+            exit(1);
+        }
+    }
+}
 
-class http_header_response : http_header{
-    server s;
-};
+//_________________________________________
+
 
 class http_reqest{
-    http_header_reqest r;
-    std::vector<http_header> http_h;
-    string body;
-};
-
-class http_response_head{
-    http_header_response r;
-    std::vector<http_header> http_h;
-};
-
-class http_response_get : http_response_head{
-    string body;
-};
-
-class http_server{
+    //http_header_reqest r;
+    //std::vector<http_header> http_h;
+    //string body;
+    string hr;
 public:
-    http_response_get GET(http_header_reqest& h){};
-    http_response_head HEAD(http_header_reqest& h){};
+    http_reqest() {
+        //hr = "GET /cgi-bin/cgi?NAME=Tanya&SURNAME=D HTTP/1.1\r\0"; //check
+        hr = "GET /index.html HTTP/1.1\r\0"; //check
+    }
+    string str() const {
+        return hr;
+    }
 };
 
-class http_client{
-};
 
-//<схема>://<хост><путь к файлу-ресурсу>[?<параметры запроса>] - URI
-
-*/
+#endif //SERVER_SERLIB_H
